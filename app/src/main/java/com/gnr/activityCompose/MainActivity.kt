024fffb5d3c133ed510384d6a311410390cfbefb
Genner <lopezitzepcgenneralexander@gmail.com>
2024-08-26 11:52:17 -0600

    Error // Barra que se superpone al resto del contenido

--- app/src/main/java/com/gnr/activityCompose/MainActivity.kt
+++ app/src/main/java/com/gnr/activityCompose/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.gnr.activityCompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,10 +15,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -26,21 +31,40 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.gnr.activityCompose.ui.theme.HelloWorldComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            App()
+            ViewContainer()
         }
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview
 @Composable
-fun App() {
+fun ViewContainer(){
+    Scaffold (
+        topBar = {Toolbar()},
+        content = { paddingValues ->
+            Content(modifier = Modifier.padding(paddingValues))
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Toolbar(){
+    TopAppBar(title = { Text(text = "GennerDevProfile")})
+}
+
+@Composable
+fun Content(modifier: Modifier = Modifier) {
+
+    var counter by rememberSaveable { mutableStateOf(0) }
+
     LazyColumn(
         modifier = Modifier
             .background(Color.Gray)
@@ -51,34 +75,68 @@ fun App() {
             Image(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(400.dp),
+                    .height(500.dp),
                 painter = painterResource(id = R.drawable.top),
                 contentDescription = null
             )
+            Row (modifier = Modifier.padding(top = 8.dp, start = 5.dp)){
+                Image(
+                    painter = painterResource(id = R.drawable.ic_favorite),
+                    contentDescription = "like",
+                    modifier = Modifier.clickable { counter++ }
+                )
+                Text(text = counter.toString(), color = Color.White, modifier = Modifier.padding(start = 4.dp))
+            }
             Text(
-                text = "Genner",
+                text = "GennerDev",
                 fontSize = 32.sp,
                 color = Color.White,
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
                 textAlign = TextAlign.Center
             )
-            Text(text = "Compose", color = Color.White)
-            Text(text = "Android", color = Color.White)
+            Text(text = "Android Dev", color = Color.White, modifier = Modifier.padding(top = 8.dp))
+            Text(
+                text = "2 Semanas exp",
+                color = Color.White,
+                modifier = Modifier.padding(top = 5.dp)
+            )
             LazyRow(
-                horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(16.dp)
+                    .padding(top = 5.dp)
             ) {
                 item {
-                    Text(text = "Stack: ", color = Color.White)
-                    Text(text = "Kotlin", color = Color.White)
-                    Text(text = "Python", color = Color.White)
-                    Text(text = "Js", color = Color.White)
-                    Text(text = "Flet", color = Color.White)
+                    Text(
+                        text = "Stack: ",
+                        color = Color.White,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Text(
+                        text = "Kotlin",
+                        color = Color.White,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Text(
+                        text = "Python",
+                        color = Color.White,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
+                    Text(text = "Js", color = Color.White, modifier = Modifier.padding(end = 8.dp))
+                    Text(
+                        text = "Flet",
+                        color = Color.White,
+                        modifier = Modifier.padding(end = 8.dp)
+                    )
                     Text(text = "JAVA", color = Color.White)
                 }
             }
         }
     }
 }
+
+
+
+
+
